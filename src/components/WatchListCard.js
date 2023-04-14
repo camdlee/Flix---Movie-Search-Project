a7f8b2ebec5673187de6dedf7b642467764d9c56
Cameron Lee <camdlee18@gmail.com>
2023-04-14 10:06:32 -0400

    added genres display to watch list cards

--- src/components/WatchListCard.js
+++ src/components/WatchListCard.js
@@ -29,6 +29,42 @@ export default function WatchListCard(watchList) {
     p: 4,
   };
 
+  //-------------------------- Display Genres ----------------------------------
+  //{genres} prop is an array of genre ids, so we have to parse through the array and match it to the dictionary we have of genre ids
+  const genres_dict = {
+    28: 'Action',
+    12: 'Adventure',
+    16: 'Animation',
+    35: 'Comedy',
+    80: 'Crime',
+    99: 'Documentary',
+    18: 'Drama',
+    10751: 'Family',
+    10762: 'Kids',
+    14: 'Fantasy',
+    36: 'History',
+    27: 'Horror',
+    10402: 'Music',
+    9648: 'Mystery',
+    10763: 'News',
+    10764: 'Reality',
+    10749: 'Romance',
+    10765: 'Sci-Fi & Fantasy',
+    10766: 'Soap',
+    10767: 'Talk',
+    10768: 'War & Politics',
+    878: 'Science Fiction',
+    10770: 'TV Movie',
+    53: 'Thriller',
+    10752: 'War',
+    37: 'Western'
+  }
+  const movieGenres = []
+  for (let genre of watchList.watchList.genres) {
+    movieGenres.push(genres_dict[genre])
+  }
+
+  //-------------------------- Modal -------------------------------------------
   const [open, setOpen] = React.useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -77,6 +113,12 @@ export default function WatchListCard(watchList) {
                             </Typography>
                             <Typography variant="body2" color="text.secondary">
                                 Released Date: {watchList.watchList.releaseDate}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary">
+                                Genres: {movieGenres.map(genre => {
+                                        return(genre+" | ")
+                                        }
+                                        )}
                             </Typography>
                             <Typography id="modal-modal-description" sx={{ mt: 2 }}>
                                 Desciption: {watchList.watchList.description}
