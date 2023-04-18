3f8c2112691d004be0245524e5aa3fbede5256af
Cameron Lee <camdlee18@gmail.com>
2023-04-17 20:07:43 -0400

    cleaned up navbar to show/hide when user logged in

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -133,7 +133,7 @@ export default function Footer() {
         {/* Copyright */}
         <div className="text-center p-3" style={{backgroundColor: 'rgba(0, 0, 0, 0.2)'}}>
         © 2023 Copyright:
-        <a className="text-blue" href="https://github.com/camdlee"> Cameron Lee</a>
+        <a className="text-blue" href="https://github.com/camdlee" target='_blank'> Cameron Lee</a>
         </div>
         {/* Copyright */}
     </footer>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
+import { useState, useEffect } from 'react';
 import AuthState from './AuthState';
-import Dropdown from 'react-bootstrap/Dropdown';
-// import Nav from './Nav.css';
+import { auth } from '../firebase';
+import { onAuthStateChanged } from "firebase/auth";
 
 
 
 export default function Navbar(){
+
+    const [authUser, setAuthUser] = useState('')
+
+    useEffect(() => {
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setAuthUser(user)
+                console.log(user)
+                const uid = user.uid;
+                // ...
+            } else {
+              setAuthUser('')
+            }  
+    }, [authUser])
+    })
+
+    console.log(authUser)
 
     return (
         <div className="nav-div">
@@ -30,48 +48,55 @@ export default function Navbar(){
                     <li className="nav-item">
                         <Link className="nav-link fancy-link" to="/">Home<span className="sr-only">(current)</span></Link>
                     </li>
-                    <li className="nav-item">
-                        <Link className="nav-link fancy-link" to="/signup">
-                            Sign Up
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link fancy-link" to="/signin">
-                            Sign In
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link fancy-link" to="/movie">
-                            Explore
-                        </Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link fancy-link" to="/watchlist">
-                            Watch List
-                        </Link>
-                    </li>
+                    { authUser ? 
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link fancy-link" to="/movie">
+                                    Explore
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link fancy-link" to="/watchlist">
+                                    Watch List
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <AuthState/>
+                            </li>
+                            <li className="nav-item" sx={{ p: 0}}>
+                                <Link className="nav-link" to="profile" sx={{ p:0 }}>
+                                    {/* <Dropdown>
+                                        <Dropdown.Toggle id="dropdown-basic"> */}
+                                            <Avatar className="avatar-icon" href="/profile" alt="User" src="https://pbs.twimg.com/media/EwnMiM2WgAQAH6y.jpg" / >
+                                        {/* </Dropdown.Toggle>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item>Action</Dropdown.Item>
+                                            <Dropdown.Item>Adventure</Dropdown.Item>
+                                            <Dropdown.Item>Animation</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown> */}
+                                </Link>
+                            </li>
+                        </ul>
+                        :
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link fancy-link" to="/signup">
+                                    Sign Up
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link fancy-link" to="/signin">
+                                    Sign In
+                                </Link>
+                            </li>
+                        </ul>
+                    }
                     {/* <li className="nav-item">
                         <Link className="nav-link" to="/profile">
                             Profile
                         </Link>
                     </li> */}
-                    <li className="nav-item">
-                        <AuthState/>
-                    </li>
-                    <li className="nav-item" sx={{ p: 0}}>
-                        <Link className="nav-link" to="profile" sx={{ p:0 }}>
-                            {/* <Dropdown>
-                                <Dropdown.Toggle id="dropdown-basic"> */}
-                                    <Avatar className="avatar-icon" href="/profile" alt="User" src="https://pbs.twimg.com/media/EwnMiM2WgAQAH6y.jpg" / >
-                                {/* </Dropdown.Toggle>
-                                <Dropdown.Menu>
-                                    <Dropdown.Item>Action</Dropdown.Item>
-                                    <Dropdown.Item>Adventure</Dropdown.Item>
-                                    <Dropdown.Item>Animation</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown> */}
-                        </Link>
-                    </li>
                 </ul>
                 </div>
             </nav>
