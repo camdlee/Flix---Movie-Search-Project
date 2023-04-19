655efcf6cb15441e410aaeee74a898f08fd9dc24
Cameron Lee <camdlee18@gmail.com>
2023-04-18 20:59:42 -0400

    started profile page

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,18 +1,193 @@
-import React from 'react'
+import React from 'react';
+import { auth } from '../firebase';
+import { useState, useEffect } from 'react';
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 export default function ProfileCard() {
+
+  const [authUser, setAuthUser] = useState('')
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            setAuthUser(user)
+            console.log(user)
+            const uid = user.uid;
+            // ...
+        } else {
+          setAuthUser('')
+        }  
+      }, [authUser])
+      })
+
   return (
-    <div className='profile-card'>
-        <div className='profile-upper-container'>
-            <div className='profile-img-container'>
-                <img src={require('../static/profile-pics/trixie-mattel.jpg').default } alt="profile-pic" height="100px" width="100px"/>
-            </div>url
+    <div>
+      <section>
+        <div className="container py-5">
+          {/* <div className="row">
+            <div className="col">
+              <nav aria-label="breadcrumb" className="bg-light rounded-3 p-3 mb-4">
+                <ol className="breadcrumb mb-0">
+                  <li className="breadcrumb-item"><a href="#">Home</a></li>
+                  <li className="breadcrumb-item"><a href="#">User</a></li>
+                  <li className="breadcrumb-item active" aria-current="page">User Profile</li>
+                </ol>
+              </nav>
+            </div>
+          </div> */}
+          <div className="row">
+            <div className="col-lg-4">
+              <div className="card mb-4">
+                <div className="card-body text-center">
+                  <img src="https://pbs.twimg.com/media/EwnMiM2WgAQAH6y.jpg" alt="avatar" className="rounded-circle img-fluid" style={{width: 150}} />
+                  <h5 className="my-3">{authUser.displayName}</h5>
+                  <p className="text-muted mb-1">All Stars 3 Winner</p>
+                  <p className="text-muted mb-4">Los Angeles, CA</p>
+                  <div className="d-flex justify-content-center mb-2">
+                    <button type="button" className="btn btn-outline-primary">Edit</button>
+                    {/* <button type="button" className="btn btn-outline-primary ms-1">Message</button> */}
+                  </div>
+                </div>
+              </div>
+              <div className="card mb-4 mb-lg-0">
+                <div className="card-body p-0">
+                  <ul className="list-group list-group-flush rounded-3">
+                    <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                      <i className="fas fa-globe fa-lg text-warning" />
+                      <p className="mb-0">Github</p>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                      <i className="fab fa-github fa-lg" style={{color: '#333333'}} />
+                      <p className="mb-0">LinkedIn</p>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                      <i className="fab fa-twitter fa-lg" style={{color: '#55acee'}} />
+                      <p className="mb-0">Instagram</p>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                      <i className="fab fa-instagram fa-lg" style={{color: '#ac2bac'}} />
+                      <p className="mb-0">Twitter</p>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center p-3">
+                      <i className="fab fa-facebook-f fa-lg" style={{color: '#3b5998'}} />
+                      <p className="mb-0">Facebook</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-8">
+              <div className="card mb-4">
+                <div className="card-body">
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <p className="mb-0">Full Name</p>
+                    </div>
+                    <div className="col-sm-9">
+                      <p className="text-muted mb-0">{authUser.displayName}</p>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <p className="mb-0">Email</p>
+                    </div>
+                    <div className="col-sm-9">
+                      <p className="text-muted mb-0">{authUser.email}</p>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <p className="mb-0">Phone</p>
+                    </div>
+                    <div className="col-sm-9">
+                      <p className="text-muted mb-0">(097) 234-5678</p>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <p className="mb-0">Mobile</p>
+                    </div>
+                    <div className="col-sm-9">
+                      <p className="text-muted mb-0">(098) 765-4321</p>
+                    </div>
+                  </div>
+                  <hr />
+                  <div className="row">
+                    <div className="col-sm-3">
+                      <p className="mb-0">Address</p>
+                    </div>
+                    <div className="col-sm-9">
+                      <p className="text-muted mb-0">Los Angeles, CA</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="card mb-4 mb-md-0">
+                    <div className="card-body">
+                      <p className="mb-4"><span className="text-primary font-italic me-1">assigment</span> Project Status
+                      </p>
+                      <p className="mb-1" style={{fontSize: '.77rem'}}>Web Design</p>
+                      <div className="progress rounded" style={{height: 5}}>
+                        <div className="progress-bar" role="progressbar" style={{width: '80%'}} aria-valuenow={80} aria-valuemin={0} aria-valuemax={100} />
+                      </div>
+                      <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>Website Markup</p>
+                      <div className="progress rounded" style={{height: 5}}>
+                        <div className="progress-bar" role="progressbar" style={{width: '72%'}} aria-valuenow={72} aria-valuemin={0} aria-valuemax={100} />
+                      </div>
+                      <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>One Page</p>
+                      <div className="progress rounded" style={{height: 5}}>
+                        <div className="progress-bar" role="progressbar" style={{width: '89%'}} aria-valuenow={89} aria-valuemin={0} aria-valuemax={100} />
+                      </div>
+                      <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>Mobile Template</p>
+                      <div className="progress rounded" style={{height: 5}}>
+                        <div className="progress-bar" role="progressbar" style={{width: '55%'}} aria-valuenow={55} aria-valuemin={0} aria-valuemax={100} />
+                      </div>
+                      <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>Backend API</p>
+                      <div className="progress rounded mb-2" style={{height: 5}}>
+                        <div className="progress-bar" role="progressbar" style={{width: '66%'}} aria-valuenow={66} aria-valuemin={0} aria-valuemax={100} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-6">
+                  <div className="card mb-4 mb-md-0">
+                    <div className="card-body">
+                      <p className="mb-4"><span className="text-primary font-italic me-1">assigment</span> Project Status
+                      </p>
+                      <p className="mb-1" style={{fontSize: '.77rem'}}>Web Design</p>
+                      <div className="progress rounded" style={{height: 5}}>
+                        <div className="progress-bar" role="progressbar" style={{width: '80%'}} aria-valuenow={80} aria-valuemin={0} aria-valuemax={100} />
+                      </div>
+                      <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>Website Markup</p>
+                      <div className="progress rounded" style={{height: 5}}>
+                        <div className="progress-bar" role="progressbar" style={{width: '72%'}} aria-valuenow={72} aria-valuemin={0} aria-valuemax={100} />
+                      </div>
+                      <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>One Page</p>
+                      <div className="progress rounded" style={{height: 5}}>
+                        <div className="progress-bar" role="progressbar" style={{width: '89%'}} aria-valuenow={89} aria-valuemin={0} aria-valuemax={100} />
+                      </div>
+                      <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>Mobile Template</p>
+                      <div className="progress rounded" style={{height: 5}}>
+                        <div className="progress-bar" role="progressbar" style={{width: '55%'}} aria-valuenow={55} aria-valuemin={0} aria-valuemax={100} />
+                      </div>
+                      <p className="mt-4 mb-1" style={{fontSize: '.77rem'}}>Backend API</p>
+                      <div className="progress rounded mb-2" style={{height: 5}}>
+                        <div className="progress-bar" role="progressbar" style={{width: '66%'}} aria-valuenow={66} aria-valuemin={0} aria-valuemax={100} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='profile-lower-container'>
-            <h3>Name</h3>
-            <h4>Title</h4>
-            <p>Description</p>
-        </div>
+      </section>
+
     </div>
   )
 }
--- src/views/UserProfile.js
+++ src/views/UserProfile.js
@@ -12,12 +12,8 @@ export default function UserProfile() {
 
 
   return (
-    <div className="welcome-card">
-      <h1>Welcome</h1>
-      <Stack direction="row" spacing={2}>
-        <Avatar alt="Remy Sharp" sxc={{ width: 100, height: 100}}src="/static/images/avatar/1.jpg" />
-      </Stack>
-      <ProfileCard></ProfileCard>
+    <div className="profile-page-container">
+        <ProfileCard/>
     </div>
   )
 }
