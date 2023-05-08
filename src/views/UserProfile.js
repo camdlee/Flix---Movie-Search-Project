db7e7ec11ae7b9faceafecd17d310a8f80b99dcf
Cameron Lee <camdlee18@gmail.com>
2023-05-07 23:12:03 -0400

    toggle edit profile button

--- src/views/UserProfile.js
+++ src/views/UserProfile.js
@@ -8,6 +8,7 @@ import { onAuthStateChanged, updateProfile, signOut } from "firebase/auth";
 import { auth } from '../firebase';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 
@@ -16,7 +17,10 @@ import Container from '@mui/material/Container';
 export default function UserProfile() {
 
   const [authUser, setAuthUser] = useState('')
-  const [editProfile, setEditProfile] = useState(false)  
+  const [edit, setEdit] = useState(false)
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [name, setName] = useState('')  
 
     // ---------------------------------useEffect hook to authenticate users-----------------------------------
     useEffect(() => {
@@ -33,9 +37,16 @@ export default function UserProfile() {
         })
 
     // ---------------------------------Update profile info function-----------------------------------
+    const editProfile = (event) =>{
+      setEdit(true)
+    }
+    const submitEditProfile = () =>{
+      setEdit(false)
+    }
+
     const handleSubmit = (event) => {
       event.preventDefault();
-
+      setEdit(false)
     }
 
   return (
@@ -47,16 +58,28 @@ export default function UserProfile() {
               width: '60%',
             }}
           >    
-          { editProfile ? 
+          { edit ? 
               <>
-                <Button type='submit' variant="outlined" color="primary">Submit</Button>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                  onChange={(event) => {setName(event.target.value)}}
+                  sx={{ borderRadius: '5px', backgroundColor: 'white'}}
+                  />
+                <Button onClick={submitEditProfile} type='submit' variant="outlined" color="primary">Submit</Button>
               </>
             :
               <div>      
                 <img src="https://pbs.twimg.com/media/EwnMiM2WgAQAH6y.jpg" alt="avatar" className="rounded-circle img-fluid" style={{width: 150}} />
                 <h2 className="my-3">{authUser.displayName}</h2>
                 <h5>{authUser.email}</h5>
-                <button type="button" className="btn btn-outline-primary">Edit</button>
+
+                <button onClick={editProfile} type="button" className="btn btn-outline-primary">Edit</button>
               </div>
           }  
       </Box>
